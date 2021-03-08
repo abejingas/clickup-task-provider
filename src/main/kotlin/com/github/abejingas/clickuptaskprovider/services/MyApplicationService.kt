61b231e18d8fcefa1a80a1853b8c6fa010bd9171
GitHub Action <action@github.com>
2021-03-08 12:43:49 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/abejingas/clickuptaskprovider/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.abejingas.clickuptaskprovider.services
+
+import com.github.abejingas.clickuptaskprovider.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/abejingas/clickuptaskprovider/services/MyProjectService.kt
+++ src/main/kotlin/com/github/abejingas/clickuptaskprovider/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.abejingas.clickuptaskprovider.services
 
+import com.github.abejingas.clickuptaskprovider.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
